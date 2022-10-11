2f02be587dedb8b192f532102c19ae32268b27a3
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-11 22:35:40 +0600

    active class added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
@@ -8,12 +9,12 @@ const Header = () => {
                 <div>
                     <h2 className='text-4xl font-bold text-teal-600'>Captise</h2>
                 </div>
-                <div className='flex flex-col sm:flex-row'>
-                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3' to='/home'>Home</NavLink>
-                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3' to='/topics'>Topics</NavLink>
-                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3' to='/stats'>Statistics</NavLink>
-                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3' to='/blogs'>Blog</NavLink>
-                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3' to='/about'>About</NavLink>
+                <div className='flex flex-col sm:flex-row nav-items'>
+                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3 px-4 py-1' to='/home'>Home</NavLink>
+                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3 px-4 py-1' to='/topics'>Topics</NavLink>
+                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3 px-4 py-1' to='/stats'>Statistics</NavLink>
+                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3 px-4 py-1' to='/blogs'>Blog</NavLink>
+                    <NavLink className='sm:text-2xl text-teal-600 my-3 mx-3 px-4 py-1' to='/about'>About</NavLink>
                 </div>
 
             </div>
